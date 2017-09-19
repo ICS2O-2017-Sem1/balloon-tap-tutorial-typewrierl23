7df30fec4a05625b51f78c46e9fc18707fbc9354
typewrierl23 <jacob.bicol@ocsbstudent.ca>
2017-09-19 11:16:33 -0400

    my frist day

--- main.lua
+++ main.lua
@@ -4,4 +4,26 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local background = display.newImageRect( "background.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect( "platform.png", 300, 50 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight-25
+
+local balloon = display.newImageRect( "balloon.png", 112, 112 )
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
+
+local physics = require( "physics" )
+physics.start()
+
+physics.addBody( platform, "static" )
+physics.addBody( balloon, "dynamic", {radius=50, bounce=0.5 })
+
+local function pushBalloon()
+   balloon:applyLinearImpulse( 0, -1, balloon.x, balloon.y )
+end
+
+balloon:addEventListener( "tap", pushBalloon )
