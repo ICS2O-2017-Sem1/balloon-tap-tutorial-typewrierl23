fe5340e84de4548956bc582cdb4bdc02d4e1669c
typewrierl23 <jacob.bicol@ocsbstudent.ca>
2017-09-20 09:43:26 -0400

    music and a background
    ok

--- main.lua
+++ main.lua
@@ -4,13 +4,25 @@
 --
 -----------------------------------------------------------------------------------------
 
-local background = display.newImageRect( "background.png", 360, 570 )
-background.x = display.contentCenterX
+local tapCount=0
+
+local background = display.newImageRect( "yellowstone-national-park-usa.png", 1764, 682 )
+background.x = display.contentHeight+50
 background.y = display.contentCenterY
+
+local music = audio.loadSound( "Maid with the Flaxen Hair.mp3"  )
+audio.play ( music )
+
+local tapText = display.newText( tapCount, display.contentCenterX, 20, native.sysemFont, 40 )
+tapText:setFillColor (0.434463,0.24134,1)
 
 local platform = display.newImageRect( "platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
+
+local platform1 = display.newImageRect( "platform.png", 300, 50 )
+platform1.x = display.contentCenterX
+platform1.y = display.contentHeight-700
 
 local balloon = display.newImageRect( "balloon.png", 112, 112 )
 balloon.x = display.contentCenterX
@@ -19,11 +31,14 @@ balloon.y = display.contentCenterY
 local physics = require( "physics" )
 physics.start()
 
-physics.addBody( platform, "static" )
-physics.addBody( balloon, "dynamic", {radius=50, bounce=0.5 })
+physics.addBody( platform, "static", {bounce=0} )
+physics.addBody( platform1, "static", {bounce=0.5} )
+physics.addBody( balloon, "dynamic", {radius=50, bounce=0 })
 
 local function pushBalloon()
-   balloon:applyLinearImpulse( 0, -1, balloon.x, balloon.y )
+   balloon:applyLinearImpulse( 0, -1.4142135623730950488, balloon.x, balloon.y )
+tapCount = tapCount + 1
+tapText.text = tapCount
 end
 
 balloon:addEventListener( "tap", pushBalloon )
