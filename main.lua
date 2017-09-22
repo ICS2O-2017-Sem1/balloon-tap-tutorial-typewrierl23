cea318497a488c3957c5bd9fe8b2dfece4d91a72
typewrierl23 <jacob.bicol@ocsbstudent.ca>
2017-09-22 09:54:55 -0400

    stop (not functioning)
    ok

--- main.lua
+++ main.lua
@@ -29,16 +29,19 @@ local platform1 = display.newImageRect( "platform.png", 300, 50 )
 platform1.x = display.contentCenterX
 platform1.y = display.contentHeight-700
 
-local balloon = display.newImageRect( "balloon.png", 112, 112 )
+local balloon = display.newImageRect ("balloon.png", 112, 112)
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
+
+local stop = display.newImageRect( "stop.png", 50,50 )
 
 local physics = require( "physics" )
 physics.start()
 
-physics.addBody( platform, "static", {bounce=0} )
-physics.addBody( platform1, "static", {bounce=1} )
+physics.addBody( platform, "static", {bounce=0.4} )
+physics.addBody( platform1, "static", {bounce=0.7} )
 physics.addBody( balloon, "dynamic", {radius=50, bounce=0 })
+physics.addBody( stop, "static" )
 
 local function pushBalloon()
    balloon:applyLinearImpulse( 0, -1.4142135623730950488, balloon.x, balloon.y )
@@ -51,7 +54,6 @@ balloon:addEventListener( "tap", pushBalloon )
 local function reset()
   tapCount =0
   tapText.text = tapCount
-  
 end
 
 platform:addEventListener("collision", reset)
